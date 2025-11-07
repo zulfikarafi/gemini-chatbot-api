bd7713a2dc256e18df38725178a6d52025adea37
zulfikarafi <rafi.ahmadz1997@gmail.com>
2025-11-07 21:37:53 +0700

    update BE: change directory path public

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import { GoogleGenAI } from "@google/genai";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -13,7 +18,10 @@ const GEMINI_MODEL = "gemini-2.5-flash";
 app.use(cors());
 app.use(express.json());
 // Serve static files from the "public" directory and to show index.html on browser
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+// serve all files in the public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Endpoint
